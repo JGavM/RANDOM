6b4a35df9336cbed39f10bc3121c65fa75623eef
JGavM <42217000+JGavM@users.noreply.github.com>
2018-10-16 18:14:17 -0500

    Add files via upload

---
+++ pruebausuarios/Clases/Ventanas.java
@@ -0,0 +1,16 @@
+
+package pruebausuarios.Clases;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ventanas {
+    
+        public static Juego vJuego;
+        
+        public Ventanas(){
+            vJuego = new Juego();
+            vJuego.setTitle("¡Adivina el número!");
+        }
+}
